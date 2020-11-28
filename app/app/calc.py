c69173bf36dafdd1e24c2951133a1c0154925ee8
muz110 <38529013+muz110@users.noreply.github.com>
2020-11-28 20:40:45 +0400

    Updated admin to support custom user model.

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,7 @@
-def add(x,y):
+def add(x, y):
     """Add tow number together"""
     return x+y
     
-def subtract(x,y):
+def subtract(x, y):
     """sbtract x from y and return value"""
     return y-x
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,20 +6,20 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successfull"""
-        email = 'test@gmail.com'
-        password= 'Testpass123'
-        user = get_user_model().objects.create_user(
-        email = email,
+        email='test@gmail.com'
+        password='Testpass123'
+        user=get_user_model().objects.create_user(
+        email=email,
         password=password
         )
 
-        self.assertEqual(user.email,email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
         """Test the email for fa new user is normalized"""
-        email = 'test@GMAIL.COM'
-        user = get_user_model().objects.create_user(email, 'test123')
+        email='test@GMAIL.COM'
+        user=get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
@@ -30,7 +30,7 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """"Test creating a new superuser"""
-        user = get_user_model().objects.create_superuser(
+        user=get_user_model().objects.create_superuser(
             'test@gmail.com',
             'test123'
         )
